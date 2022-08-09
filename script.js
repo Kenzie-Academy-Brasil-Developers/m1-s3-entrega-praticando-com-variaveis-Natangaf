a94591d997e23f145031f521bf53024545306b0e
Natangaf <natanfernandes52@gmail.com>
2022-08-09 15:10:34 -0300

    version 0.9

---
+++ script.js
@@ -0,0 +1,15 @@
+let nome = "Jhon"
+
+let sobrenome = "Doyle"
+
+let nomeCompleto = `${nome} ${sobrenome}`
+
+let anoNascimento = 1992
+
+let anoAtual = 2022
+
+let idade = anoAtual - anoNascimento
+
+let cidade = "Curitiba"
+
+console.log(`Eu sou o ${nomeCompleto} tenho ${idade} anos e moro em ${cidade}`)
